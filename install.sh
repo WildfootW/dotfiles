c66467b9f7a874d6e1f219e3370c61e92dea099d
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-02-06 11:03:20 +0800

    add if before link dotfiles

--- install.sh
+++ install.sh
@@ -11,18 +11,23 @@ git_email="wild.foot.yee.tzwu@gmail.com"
 git_name="WildfootW"
 
 function install_file() {
-    dst=~/.$1
+    dst="$HOME/.$1"
     if [ -f $dst ] || [ -d $dst ]; then
         echo "File conflict: $dst"
     else
         src="$SCRIPTPATH/$1"
-        link "$src" "$dst"
+        echo "Link $src to $dst"
+        ln -s $src $dst
     fi
 }
 
-function link() {
-    echo "Link '$1' to '$2'"
-    ln -s "$1" "$2"
+function install_dotfiles_folder() {
+    if [ -e "$HOME/dotfiles" ]; then
+        echo "dotfiles in home directory existed"
+    else
+        echo "Link dotfile to home directory"
+        ln -s $SCRIPTPATH $HOME
+    fi
 }
 
 function check_software() {
@@ -36,12 +41,12 @@ function check_software() {
 }
 
 function set_git_environment_settings() {
-	git config --global user.email $git_email
-	git config --global user.name $git_name
-	git config --global color.ui true
-	git config --global core.editor vim 
-	git config --global alias.co commit
-	git config --global alias.lg "log --color --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+    git config --global user.email $git_email
+    git config --global user.name $git_name
+    git config --global color.ui true
+    git config --global core.editor vim 
+    git config --global alias.co commit
+    git config --global alias.lg "log --color --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 }
 
 echo ""
@@ -74,12 +79,11 @@ git submodule update
 #set git environment settings
 set_git_environment_settings
 
-#install files
+#install files and folders
 for file in `echo $files | tr ' ' '\n'`; do
     install_file $file
 done
-echo "link dotfile to home directory"
-ln -s  $SCRIPTPATH ~/
+install_dotfiles_folder
 
 #install vim plugins
 echo "Install vim plugins"
