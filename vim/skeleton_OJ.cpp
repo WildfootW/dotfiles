02f988ad4de16e5a8b182d3b1af72b0bec108fca
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-03-07 23:02:12 +0800

    add time

--- vim/skeleton_OJ.cpp
+++ vim/skeleton_OJ.cpp
@@ -3,10 +3,15 @@
 // https://github.com/Wildfoot
 
 #include <iostream>
+#include <ctime>
+
+#define INF 2147483647
 
 using namespace std;
 
 int main()
 {
+
+    cout << "Time used = " << (double)clock() / CLOCKS_PER_SEC << endl;
     return 0;   
 }
