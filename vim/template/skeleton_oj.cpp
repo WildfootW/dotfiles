1c69637402eea1e7e50a7699b469c1a7ada3de00
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-03-09 16:38:31 +0800

    add io optimization

--- vim/template/skeleton_oj.cpp
+++ vim/template/skeleton_oj.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(false);
 
     cout << "Time used = " << (double)clock() / CLOCKS_PER_SEC << endl;
     return 0;   
