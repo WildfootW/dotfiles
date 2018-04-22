9e231873d9c0bb7650176230516f767116cd4d02
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-04-22 17:38:13 +0800

    add create directory automatically

---
+++ make_my_workplace_dir.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#   Version 
+#   Author: WildfootW
+#   GitHub: github.com/Wildfoot
+#   Copyright (C) 2018 WildfootW All rights reserved.
+#
+
+mkdir -p ~/workplace
+mkdir -p ~/workplace/clone-project
+mkdir -p ~/workplace/work-on-project
+mkdir -p ~/workplace/Capture-the-flag
+mkdir -p ~/workplace/conference\ \&\ workshop
+mkdir -p ~/Programfile
+mkdir -p ~/Programfile/bin
