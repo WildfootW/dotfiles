01735daa60e4f49d653f650d74673fb08f59cfd8
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-04-27 22:54:16 +0800

    modify skeleton_pwn

--- vim/template/skeleton_pwn.py
+++ vim/template/skeleton_pwn.py
@@ -1,8 +1,9 @@
-#
+# not yet completed
 
 from pwn import *
 
 context.update(arch="i386", os="linux")
+context.log_level = "debug"
 
 host = "127.0.0.1"
 port = 8888
@@ -10,12 +11,11 @@ port = 8888
 #port = 
 r = remote(host, port)
 
-input("attach in gdb and press Enter")
+input("Attach in gdb and press Enter")
 
-payload = flat([])
-
-print(payload)
+#payload = flat([])
+#print(payload)
 
 #r.recvuntil(":")
 #r.sendline(payload)
-#r.interactive()
+r.interactive()
