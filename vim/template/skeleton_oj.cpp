c4b49fcd276e417bc1f1a44b38cd5e0335c79db0
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-05-24 10:11:11 +0800

    skeleton_oj add _fixstr

--- vim/template/skeleton_oj.cpp
+++ vim/template/skeleton_oj.cpp
@@ -5,8 +5,27 @@
 
 #define INF 2147483647
 #define EPS 1e-9
+#define DEFAULT_FIXSTR 3
 
 using namespace std;
+
+inline string _fixstr(string para, int alignment_num = DEFAULT_FIXSTR)
+{
+    para.resize(alignment_num, ' ');
+    return para;
+}
+inline string _fixstr(char para, int alignment_num = DEFAULT_FIXSTR)
+{
+    string ret = string(1, para);
+    return _fixstr(ret, alignment_num);
+}
+inline string _fixstr(int para, int alignment_num = DEFAULT_FIXSTR)
+{
+    string ret = to_string(para);
+    return _fixstr(ret, alignment_num);
+}
+
+/* code start here */
 
 int main()
 {
