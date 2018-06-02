650bbf14194b09e95383444f90a7ee0adb27f63b
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-06-02 14:27:09 +0800

    update skeleton.sh

---
+++ vim/template/skeleton_.sh
@@ -0,0 +1,4 @@
+# Absolute path to this script, e.g. /home/user/Pwngdb/install.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/Pwngdb
+SCRIPTPATH=$(dirname "$SCRIPT")
