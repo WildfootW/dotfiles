2a9a822d87ad9d28fc219d9765b7df7d3cd98f59
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-09-22 14:47:16 +0800

    github account Wildfoot -> WildfootW

--- vim/template/header.py
+++ vim/template/header.py
@@ -2,7 +2,7 @@
 #!/usr/bin/env python
 #   Version 
 #   Author: WildfootW
-#   GitHub: github.com/Wildfoot
+#   GitHub: github.com/WildfootW
 #   Copyright (C) 2018 WildfootW All rights reserved.
 #
 
--- vim/template/header.sh
+++ vim/template/header.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #   Version 
 #   Author: WildfootW
-#   GitHub: github.com/Wildfoot
+#   GitHub: github.com/WildfootW
 #   Copyright (C) 2018 WildfootW All rights reserved.
 #
 
