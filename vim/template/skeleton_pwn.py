d579e5c9b0f5d538c9b6f6b3324fc61ab962b03f
WildfootW <wild.foot.yee.tzwu@gmail.com>
2019-01-23 10:23:01 +0900

    update pwn

--- vim/template/skeleton_pwn.py
+++ vim/template/skeleton_pwn.py
@@ -9,30 +9,31 @@ fmt = format_s()
 def print_payload(payload, message = None):
     if message != None:
         log.info(message)
-    payload = str(payload)
-    log.info("payload: " + payload)
+    log.info("payload: " + str(payload))
     log.info("payload length: " + str(len(payload)))
 
-context.arch = "i386"
+context.arch = "amd64"
 context.os = "linux"
 context.endian = "little"
 # ["CRITICAL", "DEBUG", "ERROR", "INFO", "NOTSET", "WARN", "WARNING"]
 context.log_level = "DEBUG"
 #context.terminal = ['gnome-terminal', '-x', 'sh', '-c']
-#r = process("./a.out", env={"LD_PRELOAD" : "./libc.so.6"})
 #gdb.attach(r, execute='b main\n')
 
-is_local = True
-
+is_local = False
+#is_local = True
 host = ""
 port = 0
 if is_local:
     host = "127.0.0.1"
     port = 8888
-r = remote(host, port)
+#r = remote(host, port)
+#r.close()
+#r = process("./a.out")  #r = process("./a.out", env={"LD_PRELOAD" : "./libc.so.6"})
 
 input("Attach in gdb and press Enter")
 
+#u64(r.recvuntil("\n")[:-1].ljust(8, b"\x00"))
 #r.recvuntil(":")
 #payload = flat([])
 #print_payload(payload)
