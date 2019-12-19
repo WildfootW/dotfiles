02769833f0d3d1d6d710e422c6dab3bf7223a19b
WildfootW <wildfootw@gmail.com>
2019-12-19 13:44:11 +0800

    update vim pwn template

--- vim/template/skeleton_pwn.py
+++ vim/template/skeleton_pwn.py
@@ -1,4 +1,3 @@
-# not yet completed
 
 from pwn import *
 #from my_pwn_tools import *
@@ -11,28 +10,26 @@ def print_payload(payload, message = None):
 
 context.arch = "amd64"
 context.os = "linux"
-context.endian = "little"
 # ["CRITICAL", "DEBUG", "ERROR", "INFO", "NOTSET", "WARN", "WARNING"]
 context.log_level = "DEBUG"
 context.terminal = ["tmux", "split-window"] # ["gnome-terminal", "-x", "sh", "-c"] # ["tmux", "neww"]
 
 is_local = True
 #is_local = False
-r = process("./a.out")  #r = process("./a.out", env={"LD_PRELOAD" : "./libc.so.6"})
-libc = ELF("/lib/x86_64-linux-gnu/libc-2.27.so") # ELF("/lib/i386-linux-gnu/libc-2.27.so")
-host = "127.0.0.1"
-port = 8888
+#r = process("./a.out")  #r = process("./a.out", env={"LD_PRELOAD" : "./libc.so.6"})
 #r.close()
-#r = remote(host, port)
+# ELF("/lib/x86_64-linux-gnu/libc-2.27.so") # ELF("/lib/i386-linux-gnu/libc-2.27.so")
+libc = ELF("./libc.so")
+host = "localhost"
+port = 7071
 if not is_local:
     host = ""
     port = 0
-    r = remote(host, port)
-    libc = ELF("./libc-2.23.so")
+r = remote(host, port)
 
-if is_local:
-    #input("Attach in gdb and press Enter")
-    gdb.attach(r, execute='b main\n')
+#if is_local:
+#    #input("Attach in gdb and press Enter")
+#    gdb.attach(r, execute='b main\n')
 
 #u64(r.recvuntil("\n")[:-1].ljust(8, b"\x00"))
 #r.recvuntil(":")
@@ -41,3 +38,4 @@ if is_local:
 #r.sendline(payload)
 
 r.interactive("Pwned # ")
+
