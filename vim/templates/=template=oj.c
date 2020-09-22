54b41e143f3a942ae8055d16f1be54581ba08d70
WildfootW <wildfootw@wildfoo.tw>
2020-09-22 16:05:33 +0800

    move to new template

---
+++ vim/templates/=template=oj.c
@@ -0,0 +1,1 @@
+=template=oj.cpp
---
+++ vim/templates/=template=oj.cpp
@@ -0,0 +1,18 @@
+/*
+ * %FFILE%
+ * Copyright (C) %YEAR% %USER% <%MAIL%>
+ *
+ * Distributed under terms of the %LICENSE% license.
+ */
+
+// Wrong Answer
+
+#define INF 2147483647
+#define EPS 1e-9
+
+int main()
+{
+
+%HERE%
+    return 0;
+}
