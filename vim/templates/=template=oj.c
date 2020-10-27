de831763d107ad350ee19378a0ed2a1bee166b1a
WildfootW <wildfootw@wildfoo.tw>
2020-10-27 18:19:29 +0800

    modify Template

--- vim/templates/=template=oj.c
+++ vim/templates/=template=oj.c
@@ -1,1 +1,18 @@
-=template=oj.cpp
+/*
+ * %FFILE%
+ * Copyright (C) %YEAR% %USER% <%MAIL%>
+ *
+ * Distributed under terms of the %LICENSE% license.
+ */
+
+// Wrong Answer
+
+#define INF 2147483647
+#define EPS 1e-9
+
+int main()
+{
+
+%HERE%
+    return 0;
+}
--- vim/templates/=template=oj.cpp
+++ vim/templates/=template=oj.cpp
@@ -7,12 +7,40 @@
 
 // Wrong Answer
 
+#include <iostream>
+#include <ctime>
+
 #define INF 2147483647
 #define EPS 1e-9
+#define DEFAULT_FIXSTR 3
+
+using namespace std;
+
+inline string _fixstr(string para, int alignment_num = DEFAULT_FIXSTR)
+{
+    para.resize(alignment_num, ' ');
+    return para;
+}
+inline string _fixstr(char para, int alignment_num = DEFAULT_FIXSTR)
+{
+    string ret = string(1, para);
+    return _fixstr(ret, alignment_num);
+}
+inline string _fixstr(int para, int alignment_num = DEFAULT_FIXSTR)
+{
+    string ret = to_string(para);
+    if(para == INF)
+        ret = "INF";
+    return _fixstr(ret, alignment_num);
+}
+
+%HERE%
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-%HERE%
+    clog << "Time used = " << (double)clock() / CLOCKS_PER_SEC << endl;
     return 0;
 }
